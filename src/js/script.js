b4d3dabf41d168d795334b15552fe748c3e1e87d
Joey Dye <joey.m.dye@gmail.com>
2018-06-14 16:37:59 -0500

    feat: Add carousel component

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const elems = document.querySelectorAll('.scrollspy');
   const instances = M.ScrollSpy.init(elems);
 });
+
+// Init Carousel
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+    indicators: true
+  });
+});
